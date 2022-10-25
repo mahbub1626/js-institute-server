ddb47b9fdaa3737849e25d489bc5676694870bc5
mahbub1626 <mahbub1626@gmail.com>
2022-10-25 20:40:30 +0600

    course category data added

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+
+
+const categories = require('./data/coursesCategory.json');
+const course = require('./data/courses.json')
+
+
 
 const port = 5000;
 
 app.get('/', (req, res)=>{
     res.send('Hello world')
+})
+app.get('/categories', (req, res)=>{
+    res.send(categories);
 })
 
 app.listen(port, ()=>{
