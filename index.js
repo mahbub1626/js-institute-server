426eb5a8ecb62369a64155c0bdd09711f3481d43
mahbub1626 <mahbub1626@gmail.com>
2022-10-26 10:54:36 +0600

    course date added

--- index.js
+++ index.js
@@ -2,21 +2,39 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 
+const port = process.env.PORT || 5000;
 
 const categories = require('./data/coursesCategory.json');
-const course = require('./data/courses.json')
+const courses = require('./data/courses.json')
 
-get.use(cors())
+app.use(cors())
 
-const port = 5000;
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send('Hello world')
 })
-app.get('/categories', (req, res)=>{
+app.get('/categories', (req, res) => {
     res.send(categories);
 })
 
-app.listen(port, ()=>{
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    const category = news.filter(n => n.category_id === id);
+    res.send(category);
+
+})
+
+app.get('/courses', (req, res)=>{
+    res.send(courses);
+})
+
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id;
+    // console.log(id);
+    const selectedCourses = courses.find(c => c._id === id);
+    res.send(selectedCourses);
+})
+
+app.listen(port, () => {
     console.log(`JS listener port: ${port}`)
 })
